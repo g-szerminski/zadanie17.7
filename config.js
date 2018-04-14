fea8e5e23867185bd526c590887897a71ae50564
Grzegorz Szermiński <szerman@poczta.fm>
2018-04-14 14:50:34 +0200

    Add google keys

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '252012385551-itt0mrk4m59u3c29f9t8t06i0t53c83r.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'wlNZMG4kJcqtS54mpIGjN9Mj',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
